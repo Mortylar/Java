64aecb1a0f2ad7ec1868abbd0fa3592bb844e44d
Morty Laree <mortylar@si-d8.nsk.21-school.ru>
2024-07-27 16:05:07 +0700

    ex05 - add Students_map module

---
+++ src/ex05/Program.java
@@ -0,0 +1,12 @@
+import schedule.Students;
+import java.util.*;
+
+
+class Program {
+
+  public static void main(String[] args) {
+		Students st = new Students(Arrays.asList("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"));
+		st.Print();
+		//stat.PrintStatistic();
+  }
+}
---
+++ src/ex05/schedule/Students.java
@@ -0,0 +1,83 @@
+package schedule;
+
+import java.util.*;
+
+public class Students {
+	public Students() {
+	  BaseInitialization();
+	}
+
+	public Students(List<String> names) {
+		BaseInitialization();
+	  int size = names.size();
+		if (size >= max_students_count_) size = max_students_count_;
+		Iterator<String> it = names.iterator();
+		while (size > 0) {
+		  students_list_.put(students_count_, it.next());
+			++students_count_;
+			--size;
+		}
+	}
+
+	public Students(Collection<String> names) {
+	  BaseInitialization();
+		int size = names.size();
+		if (size > max_students_count_) size = max_students_count_;
+		Iterator<String> it = names.iterator();
+		while (size > 0) {
+		  students_list_.put(students_count_, it.next());
+			++students_count_;
+			--size;
+		}
+	}
+
+	public int Enrollment(String name) {
+	  if ((students_count_ < max_students_count_) && (!students_list_.containsValue(name))) {
+		  students_list_.put(students_count_++, name);
+		}
+		return students_count_;
+	}
+
+	public void Expulsion(String name) {
+		Expulsion(GetID(name));
+	}
+
+	public void Expulsion(int id) {
+		if (students_list_.containsKey(id)) {
+	    students_list_.remove(id);
+			Students tmp = new Students(students_list_.values());
+			students_list_ = tmp.students_list_;
+			students_count_ = tmp.students_count_;
+		}
+	}
+
+  public int GetID(String name) {
+		for(Map.Entry<Integer, String> pair: students_list_.entrySet()) {
+		  if (pair.getValue().equals(name)) return pair.getKey();
+		}
+    return -1;
+	}
+
+
+	public void Print() {
+		if (students_count_ == 0) {
+		  System.out.print("Student list is empty\n");
+		} else {
+      students_list_.forEach((key, value) -> System.out.printf("%s\n", value));
+		}
+	}
+
+
+	private Map<Integer, String> students_list_ ;
+	private int students_count_;
+	private static final int max_students_count_ = 10;
+
+	private void BaseInitialization() {
+	  students_list_ = new HashMap<Integer, String>();
+		students_count_ = 0;
+	}
+
+
+
+
+}
