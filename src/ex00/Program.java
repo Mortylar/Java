a2c8ff64d0f1f14921d9a3d0e5d7c9f41d4d7404
Morty Laree <mortylar@si-d8.nsk.21-school.ru>
2024-08-03 16:50:00 +0700

    Styling ex00

--- src/ex00/Program.java
+++ src/ex00/Program.java
@@ -2,8 +2,8 @@ import number.Number;
 
 class Program {
 
-  public static void main(String[] args) {
-		Number n = new Number(654321);
-    n.WriteNumber(n.DigitsSum());
-  }
+    public static void main(String[] args) {
+        Number n = new Number(654321);
+        n.writeNumber(n.digitsSum());
+    }
 }
--- src/ex00/number/Number.java
+++ src/ex00/number/Number.java
@@ -4,37 +4,40 @@ import java.util.Scanner;
 
 public class Number {
 
-  private int number_;
+    private int number_;
 
-	public Number() {
-	  SetNumber(0);
-	}
+    public Number() {
+        setNumber(0);
+    }
 
-	public Number(int number) {
-	  SetNumber(number);
-	}
+    public Number(int number) {
+        setNumber(number);
+    }
 
-	public void SetNumber(int number) {
-	  number_ = number;
-	}
+    public void setNumber(int number) {
+        number_ = number;
+    }
 
-	public int ReadNumber() {
-	  Scanner scanner = new Scanner(System.in);
-		return scanner.nextInt();
-	}
+    public int readNumber() {
+        Scanner scanner = new Scanner(System.in);
 
-	public void WriteNumber(int number) {
-	  System.out.println(number);
-	}
+        return scanner.nextInt();
+    }
 
-	public int DigitsSum() {
-		int tmp = number_;
-		int result = 0;
-		while (tmp > 0) {
-		  result += tmp%10;
-			tmp = tmp/10;
-		}
-	  return result;
-	}
+    public void writeNumber(int number) {
+        System.out.println(number);
+    }
+
+    public int digitsSum() {
+        int tmp = number_;
+        int result = 0;
+
+        while (tmp > 0) {
+            result += tmp % 10;
+            tmp = tmp / 10;
+        }
+
+        return result;
+    }
 
 }
