f6f5d7394efc9f02892b87ff02bc63ae6c8efbe4
Morty Laree <mortylar@si-d8.nsk.21-school.ru>
2024-08-04 17:25:09 +0700

    Complete with ex05

--- src/ex05/schedule/Schedule.java
+++ src/ex05/schedule/Schedule.java
@@ -10,7 +10,6 @@ public class Schedule {
     public Schedule() {
         students_ = new Students();
         timetable_ = new Timetable();
-//      attendance_ = new Attendance(students_);
     }
 
     public void createSchedule() {
@@ -18,6 +17,72 @@ public class Schedule {
         CreateTimetable();
         createAttendance();
     }
+
+    private void createStudentsList() {
+        students_.enrollment("alelyka");
+        students_.enrollment("vladiput");
+				students_.enrollment("valstain");
+        students_.enrollment("joba");
+        students_.enrollment("xijin");
+        students_.enrollment("emmacro");
+				students_.expulsion("valstain");
+				students_.enrollment("dimoniva");
+        students_.enrollment("ankara");
+    }
+
+    private void CreateTimetable() {
+        timetable_.addClass("Saturday", 13);
+        timetable_.addClass("Sunday", 13);
+        timetable_.addClass("Sunday", 15);
+    }
+
+    private void createAttendance() {
+        attendance_ = new Attendance(students_);  //day, time, status
+        attendance_.register("alelyka", 5, 13, 1);
+        attendance_.register("vladiput", 5, 13, 1);
+        attendance_.register("joba", 5, 13, -1);
+        attendance_.register("xijin", 5, 13, 1);
+        attendance_.register("emmacro", 5, 13, -1);
+        attendance_.register("dimoniva", 5, 13, 1);
+        attendance_.register("ankara", 5, 13, 1);
+
+        attendance_.register("alelyka", 6, 13, 1);
+        attendance_.register("vladiput", 6, 13, 1);
+        attendance_.register("joba", 6, 13, -1);
+        attendance_.register("xijin", 6, 13, 1);
+        attendance_.register("emmacro", 6, 13, -1);
+        attendance_.register("dimoniva", 6, 13, 1);
+        attendance_.register("ankara", 6, 13, 1);
+
+        attendance_.register("alelyka", 6, 15, -1);
+        attendance_.register("vladiput", 6, 15, 1);
+        attendance_.register("joba", 6, 15, 1);
+        attendance_.register("xijin", 6, 15, 1);
+        attendance_.register("emmacro", 6, 15, 1);
+        attendance_.register("dimoniva", 6, 15, -1);
+        attendance_.register("ankara", 6, 15, -1);
+
+        attendance_.register("alelyka", 20, 13, 1);
+        attendance_.register("vladiput", 20, 13, 1);
+        attendance_.register("joba", 20, 13, -1);
+        attendance_.register("xijin", 20, 13, -1);
+        attendance_.register("emmacro", 20, 13, -1);
+        attendance_.register("dimoniva", 20, 13, -1);
+        attendance_.register("ankara", 20, 13, -1);
+
+
+        attendance_.register("alelyka", 26, 13, -1);
+        attendance_.register("vladiput", 26, 13, 1);
+        attendance_.register("joba", 26, 13, -1);
+        attendance_.register("xijin", 26, 13, -1);
+        attendance_.register("emmacro", 26, 13, -1);
+        attendance_.register("dimoniva", 26, 13, -1);
+        attendance_.register("ankara", 26, 13, -1);
+
+        attendance_.register("vladiput", 13, 15, 1);
+
+    }
+
 
     public void print() {
         students_.print();
@@ -32,22 +97,10 @@ public class Schedule {
         printFullSchedule();
     }
 
-    private void createStudentsList() {
-        students_.enrollment("aaa");
-        students_.enrollment("bbb");
-        students_.enrollment("ccc");
-        students_.enrollment("ddd");
-        students_.enrollment("eee");
+    private void printSeparator() {
+        System.out.print(".\n");
     }
 
-    private void CreateTimetable() {
-        timetable_.addClass("Monday", 13);
-        timetable_.addClass("Thursday", 16);
-       // timetable_.addClass("Tuesday", 13);
-       // timetable_.addClass("Saturday", 17);
-       // timetable_.addClass("Friday", 13);
-       // timetable_.addClass("Friday", 15);
-    }
 
     private void printAttendance() {
         for (int i = 0; i < students_.getStudentsCount(); ++i) {
@@ -67,12 +120,11 @@ public class Schedule {
 
     private void printFullSchedule() {
         printHeader();
-        //TODO
+				printStudentsInfo();
     }
 
     private void printHeader() {
-		    System.out.print("          ");
-				//System.out.printf("|%2d:%02d %s %2d|", 13, 15, "MO", 21);
+		    System.out.printf("%10s", "");
 
         for (int classID = 0; classID < timetable_.getClassesCount(); ++classID) {
             int day = timetable_.getClassDay(classID);
@@ -81,16 +133,31 @@ public class Schedule {
 						String dayOfWeek = timetable_.getClassDayOfWeek(classID);
             System.out.printf("%2d:%02d %s %2d|", hour, minute, dayOfWeek, day);
 				}
+				System.out.print("\n");
 		}
 
-    private void createAttendance() {
-        attendance_ = new Attendance(students_);
-        attendance_.register("aaa", 1, 13, 1);
-        attendance_.register("bbb", 1, 13, -1);
-    }
+		private void printStudentsInfo() {
+		    for (int studentID = 0; studentID < students_.getStudentsCount(); ++studentID) {
+			      String studentName = students_.getName(studentID);
+				    printStudentAttendance(studentName);
+			  }
+		}
 
-    private void printSeparator() {
-        System.out.print(".\n");
-    }
+		private void printStudentAttendance(String name) {
+			  System.out.printf("%10s", name);
+		    for (int classID = 0; classID < timetable_.getClassesCount(); ++classID) {
+				    String statusString = new String();
+			      int status = attendance_.getStatus(name, timetable_.getClassDay(classID),
+                                                     timetable_.getClassTime(classID));
+				    if (status == 1) {
+				        statusString = "1";
+				    } else if (status == -1) {
+				        statusString = "-1";
+				    }
+						System.out.printf("%11s|", statusString);
+			  }
+			  System.out.print("\n");
+		}
+
 
 }
