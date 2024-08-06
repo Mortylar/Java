0f9e4919ad60cbf4637058484c2207201777c587
Morty Laree <mortylar@si-d8.nsk.21-school.ru>
2024-08-06 18:50:43 +0700

    public -> protected for setName in HomoSapiens - method call prohibition for User class

--- day01/src/ex00/Program.java
+++ day01/src/ex00/Program.java
@@ -9,25 +9,7 @@ import user.User;
 class Program {
 
     public static void main(String[] args) {
-        User user1 = new User("Vova", 1, 1);
-        User user2 = new User("Vova", 2, 1);
-
         Test test = new Test();
-        //        test.createUserList();
         test.runTest();
-
-        //        Transaction tr = createTransactionOrReturnNull(
-        //            UUID.randomUUID(), user1, user2, TransactionType.CREDIT,
-        //            -1);
-        //        if (tr == null) {
-        //            System.out.println("null");
-        //        } else {
-        //            tr.print();
-        //       }
-
-        // for (int i = 0; i < 10; ++i) {
-        //     UUID id = UUID.randomUUID();
-        //     System.out.printf("\nid = %s\n", id.toString());
-        // }
     }
 }
--- day01/src/ex00/user/HomoSapiens.java
+++ day01/src/ex00/user/HomoSapiens.java
@@ -6,7 +6,7 @@ public abstract class HomoSapiens {
     private String name_;
 
     public String getName() { return name_; }
-    public void setName(String name) { name_ = name; }
+    protected void setName(String name) { name_ = name; }
 
     public abstract void print();
 }
