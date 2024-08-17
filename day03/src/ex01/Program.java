33ba28caeadb9eafb9dd75db6a60d28b74d6a831
Morty Laree <mortylar@si-d8.nsk.21-school.ru>
2024-08-17 15:56:20 +0700

    Complete ex01

--- day03/src/ex01/Program.java
+++ day03/src/ex01/Program.java
@@ -15,13 +15,31 @@ class Program {
             System.exit(-1);
         }
 
-        int inputNumber = Integer.parseInt(
-            args[0].substring(ARGUMENT_PREFIX.length(), args[0].length()));
-    
-		    Printer printer = new Printer(inputNumber);
+        if (args[0].length() <= ARGUMENT_PREFIX.length()) {
+            System.err.printf("Incorrect argument. Expected: --count=<int>.\n");
+            System.exit(-1);
+        }
+
+        String argPrefix = args[0].substring(0, ARGUMENT_PREFIX.length());
+        if (!argPrefix.equals(ARGUMENT_PREFIX)) {
+            System.err.printf("Incorrect argument. Expected: --count=<int>.\n");
+            System.exit(-1);
+        }
+        int inputNumber = 0;
+        try {
+            inputNumber =
+                Integer.parseInt(args[0].substring(ARGUMENT_PREFIX.length()));
+        } catch (NumberFormatException e) {
+            System.err.printf("Argument is not number -> %s.\n",
+                              e.getMessage());
+        }
+
+        final int notFinalValueForNotInputNumber = inputNumber;
+
+        Printer printer = new Printer(notFinalValueForNotInputNumber);
 
         Runnable egg = () -> {
-            for (int i = 0; i < inputNumber; ++i) {
+            for (int i = 0; i < notFinalValueForNotInputNumber; ++i) {
                 printer.print(i);
             }
         };
@@ -38,19 +56,16 @@ class Printer {
 
     private int limitNumber_;
 
-    public Printer(int number) {
-        limitNumber_ = number - 1;
-		}
+    public Printer(int number) { limitNumber_ = number - 1; }
 
-		public synchronized void print(int limitNumber) {
-		  System.out.printf("%s\n", Thread.currentThread().getName());
-			try {
-          notifyAll();
-			    if (limitNumber < limitNumber_) wait();
-			} catch (InterruptedException e) {
-			    System.err.printf("%s\n", e.getMessage());
-			}
-		}
-
-
+    public synchronized void print(int limitNumber) {
+        System.out.printf("%s\n", Thread.currentThread().getName());
+        try {
+            notifyAll();
+            if (limitNumber < limitNumber_)
+                wait();
+        } catch (InterruptedException e) {
+            System.err.printf("%s\n", e.getMessage());
+        }
+    }
 }
