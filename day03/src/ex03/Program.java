2f9207cee95883491306981aa97fb57047efd093
Morty Laree <mortylar@si-d8.nsk.21-school.ru>
2024-08-17 15:30:54 +0700

    Complete ex03

--- day03/src/ex03/Program.java
+++ day03/src/ex03/Program.java
@@ -1,13 +1,31 @@
-import file.FileLoader;
-import java.lang.InterruptedException;
-import java.lang.Runnable;
-import java.lang.Thread;
-import java.util.concurrent.locks.ReentrantLock;
 
 class Program {
 
+    private static final String ARG_PREFIX = "--threadsCount=";
+    private static final int ARG_COUNT = 1;
+    private static final int ARG_INDEX = 0;
+
     public static void main(String[] args) {
-        int threadsCount = 3; // TODO
+        if (args.length != ARG_COUNT) {
+            System.err.printf("Incorrect arguments count. Expected one.\n");
+            System.exit(-1);
+        }
+
+        if (args[ARG_INDEX].length() <= ARG_PREFIX.length()) {
+            System.err.printf(
+                "Illegal argument. Expect: --threadsCount=<int>.\n");
+            System.exit(-1);
+        }
+
+        int threadsCount = 0;
+        try {
+            threadsCount = Integer.parseInt(
+                args[ARG_INDEX].substring(ARG_PREFIX.length()));
+        } catch (NumberFormatException e) {
+            System.out.printf("Argument is not number - %s.\n", e.getMessage());
+            System.exit(-1);
+        }
+
         ThreadsService service = new ThreadsService(threadsCount);
         service.run();
     }
