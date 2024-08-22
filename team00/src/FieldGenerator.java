6fc9c6f09448c1fd1c2ffffbc2b30a931cfaf7fc
Morty Laree <mortylar@si-d8.nsk.21-school.ru>
2024-08-22 17:35:17 +0700

    Start game

---
+++ team00/src/FieldGenerator.java
@@ -0,0 +1,24 @@
+
+
+public class FieldGenerator {
+
+  private char player_ = 'o';
+  private char obstakle_ = '#';
+  private char enemy_ = 'x';
+  private char target = 'O';
+
+  private int height_ = 20;
+  private int width_ = 20;
+
+  public FieldGenerator() {}
+
+  public FieldGenerator(int h, int w) {
+      height_ = h;
+      width_ = w;
+  }
+
+  public char[][] generate() {
+      //TODO
+  }
+
+}
