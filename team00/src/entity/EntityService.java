9d1343776e8c388de75299b83a26b5e613e47730
Morty Laree <mortylar@si-d8.nsk.21-school.ru>
2024-08-23 17:07:32 +0700

    Start EntityService - logic for Entities managment

---
+++ team00/src/entity/EntityService.java
@@ -0,0 +1,26 @@
+
+
+public class EntityService {
+
+    private int playersCount_;
+    private char playerIcon_;
+
+    private int enemiesCount_;
+    private char enemyIcon_;
+
+    private int obstaclesCount_;
+    private char obstacleIcon_;
+
+    private int targetsCount_;
+    private char targetIcon_
+
+    private Player[] playerArr_;
+    private Enemy[] enemyArr_;
+    private Obstacle[] obstacleArr_;
+    private Target[] targetArr_;
+
+    public void setPlayersCount(int count) {
+      
+    }
+
+}
