020c0a48c5268f0d82c61204e5ae2af1e91790c6
Morty Laree <mortylar@si-d8.nsk.21-school.ru>
2024-08-23 10:01:43 +0700

    Complete field generation

--- team00/src/Main.java
+++ team00/src/Main.java
@@ -3,8 +3,9 @@
 public class Main {
 
 
-  public static void main(String args) {
-  
+  public static void main(String[] args) {
+      FieldGenerator fGen = new FieldGenerator(20, 21);
+      fGen.generate(100, 8);
 
   }
 }
