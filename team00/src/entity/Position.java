133d49337ddd032e6b3ef71d26d9c58d024d20e4
Morty Laree <mortylar@si-d8.nsk.21-school.ru>
2024-08-23 12:46:19 +0700

    Class for position storaging

---
+++ team00/src/entity/Position.java
@@ -0,0 +1,32 @@
+public class Position {
+
+    public int x_ = 0;
+    public int y_ = 0;
+
+    public Position() {
+    }
+
+    public Position(int x, int y) {
+        x_ = x;
+        y_ = y;
+    }
+
+    public void set(int x, int y) {
+        x_ = x;
+        y_ = y;
+    }
+
+    public void move(int dx, int dy) {
+        x_ += dx;
+        y_ += dy;
+    }
+
+    public int x(() {
+        return x_;
+    }
+
+    public int y() {
+        return y_;
+    }
+
+}
