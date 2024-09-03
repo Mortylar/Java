723ab56105a0de515963241a509c380a7b22d029
Morty Laree <mortylar@si-d8.nsk.21-school.ru>
2024-09-03 18:08:01 +0700

    Create database structure

---
+++ day05/src/ex00/Chat/src/main/resources/schema.sql
@@ -0,0 +1,52 @@
+DROP TABLE IF EXISTS Users CASCADE;
+DROP TABLE IF EXISTS UserRoom CASCADE;
+DROP TABLE IF EXISTS Chatroom CASCADE;
+DROP TABLE IF EXISTS UserChatroom CASCADE;
+DROP TABLE IF EXISTS Message CASCADE;
+DROP TABLE IF EXISTS ChatroomMessages CASCADE;
+
+
+CREATE TABLE IF NOT EXISTS Users (
+                Id SERIAL PRIMARY KEY NOT null,
+                Login VARCHAR NOT null,
+                Password VARCHAR NOT null);
+
+
+
+CREATE TABLE IF NOT EXISTS UserRoom (
+                UserId INTEGER,
+                Room VARCHAR,
+                CONSTRAINT fk_UserRoom_UserId FOREIGN KEY (UserId) REFERENCES Users(Id));
+
+
+CREATE TABLE IF NOT EXISTS Chatroom (
+                Id SERIAL PRIMARY KEY NOT null,
+                Name VARCHAR,
+                OwnerId INTEGER,
+                CONSTRAINT fk_Chatroom_OwnerId FOREIGN KEY (OwnerId) REFERENCES Users(Id));
+
+
+
+CREATE TABLE IF NOT EXISTS UserChatroom (
+                UserId INTEGER,
+                ChatroomId INTEGER,
+                CONSTRAINT fk_UserChatroom_UserId FOREIGN KEY (UserId) REFERENCES Users(Id),
+                CONSTRAINT fk_UserChatroom_ChatroomId FOREIGN KEY (ChatroomId) REFERENCES Chatroom(Id));
+
+
+CREATE TABLE IF NOT EXISTS Message (
+                Id SERIAL PRIMARY KEY NOT null,
+                AutorId INTEGER,
+                RoomId INTEGER,
+                Text VARCHAR,
+                MessageTime TIMESTAMP,
+                CONSTRAINT fk_Message_AutorId FOREIGN KEY (AutorId) REFERENCES Users(Id),
+                CONSTRAINT fk_Message_RoomId FOREIGN KEY (RoomId) REFERENCES Chatroom(Id));
+
+
+CREATE TABLE IF NOT EXISTS ChatroomMessages (
+                ChatroomId INTEGER,
+                Message INTEGER,
+                CONSTRAINT fk_ChatroomMessages_ChatroomId FOREIGN KEY (ChatroomId) REFERENCES Chatroom(Id),
+                CONSTRAINT fk_ChatroomMessages_Message FOREIGN KEY (Message) REFERENCES Message(Id));
+
