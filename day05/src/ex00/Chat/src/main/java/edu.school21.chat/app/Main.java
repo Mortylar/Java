4c2e26066b60b8825e0a4641a435edddad118ff1
Morty Laree <mortylar@si-d8.nsk.21-school.ru>
2024-09-09 02:12:39 +0700

    Add DB interaction - simple demo

--- day05/src/ex00/Chat/src/main/java/edu.school21.chat/app/Main.java
+++ day05/src/ex00/Chat/src/main/java/edu.school21.chat/app/Main.java
@@ -2,15 +2,39 @@ package edu.school21.chat.app;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import edu.school21.chat.models.Reader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, IOException {
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl("jdbc:postgresql://127.0.0.1:5432/mortylar");
         config.setUsername("mortylar");
 
         HikariDataSource ds = new HikariDataSource(config);
-        System.out.printf("\nSDGEHGRHJRETRW\n");
+
+        Connection conn = ds.getConnection();
+        Statement statement = conn.createStatement();
+
+        createDataBase(statement);
+
+        ResultSet res = statement.executeQuery("SELECT * FROM Users;");
+        while (res.next()) {
+            System.out.printf(" %d ", res.getInt(1));
+            System.out.printf(" %s ", res.getString(2));
+            System.out.printf(" %s \n", res.getString(3));
+        }
+    }
+
+    public static void createDataBase(Statement statement)
+        throws IOException, SQLException {
+        Reader reader = new Reader();
+        statement.executeUpdate(reader.read("/schema.sql"));
+        statement.executeUpdate(reader.read("/data.sql"));
     }
 }
--- day05/src/ex00/Chat/src/main/resources/data.sql
+++ day05/src/ex00/Chat/src/main/resources/data.sql
@@ -16,7 +16,7 @@ SELECT Chatroom, UserId
 FROM (
     SELECT Id AS UserId, 'BRICS' AS Chatroom
     FROM Users
-    WHERE Login = 'vladiput');
+    WHERE Login = 'vladiput') AS line;
 
 
 INSERT INTO Chatroom(Name, OwnerId)
@@ -24,7 +24,7 @@ SELECT Chatroom, UserId
 FROM (
     SELECT Id AS UserId, 'NATO' AS Chatroom
     FROM Users
-    WHERE Login = 'joba');
+    WHERE Login = 'joba') AS line;
 
 
 INSERT INTO Chatroom(Name, OwnerId)
@@ -32,14 +32,14 @@ SELECT Chatroom, UserId
 FROM (
     SELECT Id AS UserId, 'Random' AS Chatroom
     FROM Users
-    WHERE Login = 'alelyka');
+    WHERE Login = 'alelyka') AS line;
 
 INSERT INTO Chatroom(Name, OwnerId)
 SELECT Chatroom, UserId
 FROM (
     SELECT Id AS UserId, 'Bathroom' AS Chatroom
     FROM Users
-    WHERE Login = 'emmacro');
+    WHERE Login = 'emmacro') AS line;
 
 
 INSERT INTO Chatroom(Name, OwnerId)
@@ -47,7 +47,7 @@ SELECT Chatroom, UserId
 FROM (
     SELECT Id AS UserId, 'Hospital' AS Chatroom
     FROM Users
-    WHERE Login = 'joba');
+    WHERE Login = 'joba') AS line;
 
 
 INSERT INTO Chatroom(Name, OwnerId)
@@ -55,7 +55,7 @@ SELECT Chatroom, UserId
 FROM (
     SELECT Id AS UserId, 'Kitchen' AS Chatroom
     FROM Users
-    WHERE Login = 'xijin');
+    WHERE Login = 'xijin') AS line;
 
 
 
@@ -65,11 +65,11 @@ SELECT UserId, ChatroomId
 FROM (
     (SELECT Id As UserId
      FROM Users
-     WHERE Login IN ('alelyka', 'vladiput', 'xijin'))
+     WHERE Login IN ('alelyka', 'vladiput', 'xijin')) AS first
     CROSS JOIN
     (SELECT Id AS ChatroomId
      FROM Chatroom
-     WHERE name = 'BRICS'));
+     WHERE name = 'BRICS') AS second);
 
 
 INSERT INTO UserChatroom(UserId, ChatroomId)
@@ -77,22 +77,22 @@ SELECT UserId, ChatroomId
 FROM (
     (SELECT Id As UserId
      FROM Users
-     WHERE Login NOT IN ('alelyka', 'vladiput', 'xijin'))
+     WHERE Login NOT IN ('alelyka', 'vladiput', 'xijin')) AS first
     CROSS JOIN
     (SELECT Id AS ChatroomId
      FROM Chatroom
-     WHERE name = 'NATO'));
+     WHERE name = 'NATO') AS second);
 
 
 INSERT INTO UserChatroom(UserId, ChatroomId)
 SELECT UserId, ChatroomId
 FROM (
     (SELECT Id As UserId
-     FROM Users)
+     FROM Users) AS first
     CROSS JOIN
     (SELECT Id AS ChatroomId
      FROM Chatroom
-     WHERE name = 'Random'));
+     WHERE name = 'Random') AS second);
 
 
 INSERT INTO UserChatroom(UserId, ChatroomId)
@@ -105,19 +105,19 @@ EXCEPT(
 --Message
 
 INSERT INTO Message(AutorId, RoomId, Text, MessageTime)
-VALUES(2, 4, "Bonjour, Do you order a pizza with cheese holes?", CURRENT_TIMESTAMP);
+VALUES(2, 4, 'Bonjour, Do you order a pizza with cheese holes?', CURRENT_TIMESTAMP);
 
 INSERT INTO Message(AutorId, RoomId, Text, MessageTime)
-VALUES(4, 4, "Non, I ordered a pizza with cheese without holes.", CURRENT_TIMESTAMP);
+VALUES(4, 4, 'Non, I ordered a pizza with cheese without holes.', CURRENT_TIMESTAMP);
 
 INSERT INTO Message(AutorId, RoomId, Text, MessageTime)
-VALUES(2, 4, "I''m sorry, but cheese is gone, only holes remain.", CURRENT_TIMESTAMP);
+VALUES(2, 4, 'I''m sorry, but cheese is gone, only holes remain.', CURRENT_TIMESTAMP);
 
 INSERT INTO Message(AutorId, RoomId, Text, MessageTime)
-VALUES(4, 4, "<censored>***</censored>", CURRENT_TIMESTAMP);
+VALUES(4, 4, '<censored>***</censored>', CURRENT_TIMESTAMP);
 
 INSERT INTO Message(AutorId, RoomId, Text, MessageTime)
-VALUES(6, 5, "Hello! Is anybody here?", CURRENT_TIMESTAMP);
+VALUES(6, 5, 'Hello! Is anybody here?', CURRENT_TIMESTAMP);
 
 
 
