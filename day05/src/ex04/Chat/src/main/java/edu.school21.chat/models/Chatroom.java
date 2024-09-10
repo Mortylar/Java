9fc4dddfd20271fb4588c52880b736e333d59b71
Morty Laree <mortylar@si-d8.nsk.21-school.ru>
2024-09-10 16:10:10 +0700

    Start ex04

---
+++ day05/src/ex04/Chat/src/main/java/edu.school21.chat/models/Chatroom.java
@@ -0,0 +1,66 @@
+package edu.school21.chat.models;
+
+import java.util.List;
+
+public class Chatroom implements ITable {
+
+    private final long chatroomId_;
+    private String name_;
+    private User owner_;
+    private List<Message> messages_;
+
+    public Chatroom(long id) { chatroomId_ = id; };
+
+    public Chatroom(long id, String name, User owner, List<Message> messages) {
+        chatroomId_ = id;
+        name_ = name;
+        owner_ = owner;
+        messages_ = messages;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if ((other == null) || !(other instanceof Chatroom)) {
+            return false;
+        }
+        if (other == this) {
+            return true;
+        }
+        return chatroomId_ == ((Chatroom)other).chatroomId_;
+    }
+
+    @Override
+    public int hashCode() {
+        return ~(int)chatroomId_;
+    }
+
+    @Override
+    public String toString() {
+        String result =
+            String.format("Chatroom %s: id = %d, owner = { %s }, messages = { ",
+                          (name_ == null) ? "null" : name_, chatroomId_,
+                          (owner_ == null) ? "null" : owner_.toString());
+        if (messages_ == null) {
+            result += "null";
+        } else {
+            for (int i = 0; i < messages_.size(); ++i) {
+                result += String.format("%s; ", messages_.get(i).toString());
+            }
+        }
+        return result += "}";
+    }
+
+    public void setName(String name) { name_ = name; }
+
+    public void setOwner(User owner) { owner_ = owner; }
+
+    public void setMessages(List<Message> messages) { messages_ = messages; }
+
+    public long getId() { return chatroomId_; }
+
+    public String setName() { return name_; }
+
+    public User setOwner() { return owner_; }
+
+    public List<Message> setMessages() { return messages_; }
+}
---
+++ day05/src/ex04/Chat/src/main/resources/schema.sql
@@ -0,0 +1,35 @@
+DROP TABLE IF EXISTS Users CASCADE;
+DROP TABLE IF EXISTS Chatroom CASCADE;
+DROP TABLE IF EXISTS UserChatroom CASCADE;
+DROP TABLE IF EXISTS Message CASCADE;
+
+
+CREATE TABLE IF NOT EXISTS Users (
+                Id SERIAL PRIMARY KEY NOT null,
+                Login VARCHAR NOT null,
+                Password VARCHAR NOT null);
+
+
+CREATE TABLE IF NOT EXISTS Chatroom (
+                Id SERIAL PRIMARY KEY NOT null,
+                Name VARCHAR,
+                OwnerId INTEGER,
+                CONSTRAINT fk_Chatroom_OwnerId FOREIGN KEY (OwnerId) REFERENCES Users(Id));
+
+
+CREATE TABLE IF NOT EXISTS UserChatroom (
+                UserId INTEGER,
+                ChatroomId INTEGER,
+                CONSTRAINT fk_UserChatroom_UserId FOREIGN KEY (UserId) REFERENCES Users(Id),
+                CONSTRAINT fk_UserChatroom_ChatroomId FOREIGN KEY (ChatroomId) REFERENCES Chatroom(Id));
+
+
+CREATE TABLE IF NOT EXISTS Message (
+                Id SERIAL PRIMARY KEY NOT null,
+                AuthorId INTEGER,
+                RoomId INTEGER,
+                Text VARCHAR,
+                MessageTime TIMESTAMP,
+                CONSTRAINT fk_Message_AutorId FOREIGN KEY (AuthorId) REFERENCES Users(Id),
+                CONSTRAINT fk_Message_RoomId FOREIGN KEY (RoomId) REFERENCES Chatroom(Id));
+
