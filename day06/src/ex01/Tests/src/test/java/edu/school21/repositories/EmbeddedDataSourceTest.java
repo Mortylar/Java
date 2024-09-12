240172e0cacab3aa75a5761878a1f71c5f705221
Morty Laree <mortylar@si-d8.nsk.21-school.ru>
2024-09-12 15:04:19 +0700

    Working with hsqldb

---
+++ day06/src/ex01/Tests/src/test/java/edu/school21/repositories/EmbeddedDataSourceTest.java
@@ -0,0 +1,10 @@
+package edu.school21.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.BeforeEach;
+
+public class EmbeddedDataSourceTest {
+
+    @BeforeEach
+    public void init() {}
+}
