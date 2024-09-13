311e13d56df0aa2b302edb2a7d892867801e2417
Morty Laree <mortylar@si-d8.nsk.21-school.ru>
2024-09-13 21:06:51 +0700

    gwefwed

--- day06/src/ex01/Tests/src/main/java/edu/school21/models/Product.java
+++ day06/src/ex01/Tests/src/main/java/edu/school21/models/Product.java
@@ -1,41 +1,26 @@
 package edu.school21.models;
 
-
 public class Product {
 
     private final long id_;
     private String name_;
-    private float price_;
+    private double price_;
 
+    public Product(long id) { id_ = id; }
 
-    public Product(long id) {
-        id_ = id;
-    }
-
-    public Product(long id, String name, float price) {
+    public Product(long id, String name, double price) {
         id_ = id;
         name_ = name;
         price_ = price;
     }
 
-    public long getId() {
-        return id_;
-    }
+    public long getId() { return id_; }
 
-    public String getName() {
-        return name_;
-    }
+    public String getName() { return name_; }
 
-    public void setName(String name) {
-        name_ = name;
-    }
+    public void setName(String name) { name_ = name; }
 
-    public float getPrice() {
-        return price_;
-    }
+    public double getPrice() { return price_; }
 
-    public void setPrice(float price) {
-        price_ = price;
-    }
-    
+    public void setPrice(double price) { price_ = price; }
 }
