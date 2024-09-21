6da9153563b80203630c424e8093a4e83ca5ecd8
Morty Laree <mortylar@si-d8.nsk.21-school.ru>
2024-09-21 14:30:01 +0700

    Remove ex00 from ex01

--- day07/src/ex01/Reflection/src/main/java/edu/school21/operation/exception/UnrecognizedOperatorException.java
+++
@@ -1,8 +0,0 @@
-package edu.school21.operation.exception;
-
-public class UnrecognizedOperatorException extends RuntimeException {
-
-    public UnrecognizedOperatorException(String operator) {
-        super(String.format("Operator '%s' does not found.\n", operator));
-    }
-}
