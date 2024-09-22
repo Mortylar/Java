2b51e03b3448d2bf0583fa97ad93c7df049e1842
Morty Laree <mortylar@si-d8.nsk.21-school.ru>
2024-09-22 23:13:38 +0700

    Update pom for annotation process in compile stage

--- day07/src/ex01/Reflection/src/main/java/edu/school21/processor/HtmlProcessor.java
+++ day07/src/ex01/Reflection/src/main/java/edu/school21/processor/HtmlProcessor.java
@@ -1,15 +1,33 @@
 package edu.school21.processor;
 
+import com.google.auto.service.AutoService;
+import java.io.FileOutputStream;
 import java.util.Set;
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
 import javax.lang.model.element.TypeElement;
 
+@AutoService(Processor.class)
+@SupportedAnnotationTypes("edu.school21.annotation.*")
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
 public class HtmlProcessor extends AbstractProcessor {
+
+    private static final String PATH = "target/file";
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations,
                            RoundEnvironment roundEnv) {
-        return true;
+        System.out.printf("\n\n\n\n\nAAAAAAAAAAaa\n\n\n\n\n");
+        try (FileOutputStream writer = new FileOutputStream(PATH)) {
+            writer.write("dfhseflkesurghlsergj".getBytes());
+            return true;
+        } catch (Exception e) {
+            System.out.printf("%s\n", e.getMessage());
+        }
+        return false;
     }
 }
